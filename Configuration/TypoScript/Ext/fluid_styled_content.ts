bb02d4f1cad268f38dccc0b1b65b0bbce9e2561c
Yannik Börgener <kontakt@boergener.de>
2019-04-21 18:00:22 +0200

    Diverses
    Diverse Anpassungen und Bilderdimensionen

---
+++ Configuration/TypoScript/Ext/fluid_styled_content.ts
@@ -0,0 +1,4 @@
+tt_content.textmedia.dataProcessing.20 {
+    maxGalleryWidth = 1200
+    maxGalleryWidthInText = 600
+}
---
+++ Configuration/TypoScript/Ext/powermail.ts
@@ -0,0 +1,4 @@
+plugin.tx_powermail.view {
+    partialRootPaths.2 = EXT:bw_dpsg_design/Resources/Ext/Powermail/Private/Partials
+    templateRootPaths.2 = EXT:bw_dpsg_design/Resources/Ext/Powermail/Private/Templates
+}
