aa72b118039d69040c48372161ec289aa3b21745
Yannik Börgener <kontakt@boergener.de>
2020-04-14 15:09:07 +0200

    TYPO3 9

---
+++ Configuration/TypoScript/constants.ts
@@ -0,0 +1,33 @@
+#Content Elemente
+styles.content {
+    textmedia {
+        maxW = 1140
+        maxWInText = 750
+    }
+
+    uploads {
+        filesizeBytesLabels = " | Kb| Mb| Gb"
+    }
+}
+
+plugin {
+    tx_bwdpsgdesign {
+        settings {
+            contact {
+                name =
+                anschrift =
+                ort =
+                telefon =
+            }
+            socialmedia {
+                facebook =
+                instagram =
+                email =
+            }
+            navigation {
+                footer_left =
+                footer_extern =
+            }
+        }
+    }
+}
